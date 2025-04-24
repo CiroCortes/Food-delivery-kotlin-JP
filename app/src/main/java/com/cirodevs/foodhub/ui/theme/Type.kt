721b77f40ffe25e18eb3a652d9c5378af5fd5080
horoscope <ciro.720@gmail.com>
2025-04-24 15:44:48 -0400

    Se inicia el proyecto y se gestiona el SplashScreen

--- app/src/main/java/com/cirodevs/foodhub/ui/theme/Type.kt
+++ app/src/main/java/com/cirodevs/foodhub/ui/theme/Type.kt
@@ -2,33 +2,61 @@ package com.cirodevs.foodhub.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cirodevs.foodhub.R
+
+// creamos ( luego de importar las font en el directorio)
+// declaramos la variable con sus fuentes principales
+val poppinsFontFamily = FontFamily(
+
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semi_bold, FontWeight.SemiBold),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_thin, FontWeight.Thin),
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_extra_light, FontWeight.ExtraLight),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_black, FontWeight.Black)
+
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppinsFontFamily, // aqui declaramos las fuentes a usar
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    ),
+    // Other default text styles to override (borramos el comentario)
+        titleLarge = TextStyle(
+        fontFamily = poppinsFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppinsFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = poppinsFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+
+
     )
-    */
+
 )
